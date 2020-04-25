44ac76a84e40b87bcf72f9c6f7bde200db7c63ed
mlaskowski <maciejlaskowsky@gmail.com>
2020-04-25 19:35:20 +0200

    add methods for handle video

--- src/render.js
+++ src/render.js
@@ -27,3 +27,62 @@ async function getVideoSources() {
   videoOptionsMenu.popup();
 }
 
+let mediaRecorder // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+// Change the videoSource window to record
+async function selectSource(source) {
+
+  videoSelectBtn.innerText = source.name;
+
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: source.id
+      }
+    }
+  }
+
+  // Create a Stream
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  // Preview the source in a video element
+  videoElement.srcObject = stream;
+  videoElement.play();
+
+  // Create the Media Recorder
+  const options = { mimeType: 'video/webm; codecs=vp9' };
+  mediaRecorder = new MediaRecorder(stream, options);
+
+  // Register Event Handlers
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+
+  // Captures all recorded chunks
+  function handleDataAvailable(e) {
+    console.log('video data available');
+    recordedChunks.push(e.data);
+  }
+
+  const { writeFile } = require('fs');
+
+  // Saves the video file on stop
+  async function handleStop(e) {
+    const blob = new Blob(recordedChunks, {
+      type: 'video/webm; codecs=vp9'
+    })
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath } = await dialog.showSaveDialog({
+      buttonLabel: 'Save video',
+      defaultPath: `vid-${Date.now()}.webm`
+    })
+
+    console.log(filePath)
+
+  }
+}
+
